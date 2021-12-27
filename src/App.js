6f9b202495cfc775529fb4cbc62b3921143ad3d1
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2021-12-27 14:01:09 -0400

    project style - done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { FiSearch } from 'react-icons/fi'
+import  './styles.css'
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input type="text" placeholder="Digite seu CEP"/>
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color='#fff' />
+        </button>
+      </div>
+
+      <main className='main'>
+        <h2>CEP: 69089-066</h2>
+        <span>Rua teste algum</span>
+        <span>Complemento: Algum complemento</span>
+        <span>Vila Rosa</span>
+        <span>Campo Grande - MS</span>
+      </main>
     </div>
   );
 }
 
-export default App;
