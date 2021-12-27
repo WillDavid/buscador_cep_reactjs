2cf5988774d0caa8b1e3267ecf0c9b5abce468ed
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2021-12-27 14:38:10 -0400

    mini projeto buscador de CEP - Finalizado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi'
 import  './styles.css'
+import api from './services/api.js'
+
+
 export default function App() {
+
+  const [input, setInput] = useState("");
+  const [cep, setCep] = useState({})
+
+  async function handleSearch(){
+    console.log(input)
+
+    if(input === ""){
+      alert("Preencha algum cep")
+      return;
+    }
+
+
+    try{
+      const response = await api.get(`${input}/json`)
+      setCep(response.data)
+      setInput('')
+    }catch{
+      alert("Ops erro ao buscar");
+      setInput('')
+    }
+  }
+
   return (
     <div className="container">
       <h1 className="title">Buscador CEP</h1>
 
       <div className="containerInput">
-        <input type="text" placeholder="Digite seu CEP"/>
+        <input type="text" placeholder="Digite seu CEP" value={input} onChange={ (e) => setInput(e.target.value) }/>
 
         <button className="buttonSearch">
-          <FiSearch size={25} color='#fff' />
+          <FiSearch size={25} color='#fff' onClick={handleSearch}/>
         </button>
       </div>
 
       <main className='main'>
-        <h2>CEP: 69089-066</h2>
-        <span>Rua teste algum</span>
-        <span>Complemento: Algum complemento</span>
-        <span>Vila Rosa</span>
-        <span>Campo Grande - MS</span>
+        <h2>CEP: {cep.cep}</h2>
+        <span>{cep.logradouro}</span>
+        <span>{cep.bairro}</span>
+        <span>{cep.localidade} - {cep.uf}</span>
       </main>
     </div>
   );
